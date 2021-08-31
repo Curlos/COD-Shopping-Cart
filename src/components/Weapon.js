65d5d7fda6c47673d518a697e219e398f72975fc
Carlos Martinez <curlosmart@gmail.com>
2021-08-31 11:31:43 -0400

    changed weapon images to local assets

--- src/components/Weapon.js
+++ src/components/Weapon.js
@@ -2,7 +2,7 @@ const Weapon = ({ weapon }) => {
 
   return (
     <div id={weapon.id} className="product">
-      <img className="productThumbImage" src={weapon['image']} alt={weapon['name']} />
+      <img className="productThumbImage" src={weapon['alt-image']} alt={weapon['name']} />
       <div className="productThumbName">{weapon['name']}</div>
       <div className="productThumbPrice">${weapon['price']}</div>
     </div>
